46088d57facac7f6b6dd918dab466d29c98b441b
Camilo A. Cáceres <camilo.caceresf@gmail.com>
2024-09-12 03:27:40 -0500

    Improved prompt and model

--- src/rag/query_rewrite.py
+++ src/rag/query_rewrite.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 
 def query_rewrite(question):
     model = Ollama(
-        model="phi3.5",
+        model="llama3.1",
         temperature=0.1
         )
 
@@ -12,7 +12,8 @@ def query_rewrite(question):
     Your task is to work with the query I will provide, where you will refine the given a search query,\
     with grammar, punctuation and clarity needed for vector search engine input.\
     In the improved query you will only answer the improved query, nothing else,\
-    do not share any thoughts, explanations or additional text.
+    do not share any thoughts, explanations or additional text. If you don't understand\
+    or you do not feel confident in your knowledge, please just answer with the Query as it is.
     
     Query: {question}
     Improved Query:
